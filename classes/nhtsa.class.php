ff355b2405d0de40e33e2057d73d869498f7a4dd
Alec M <alecmattu1234@gmail.com>
2021-04-04 16:37:28 -0400

    Add prettyDecode function

--- classes/nhtsa.class.php
+++ classes/nhtsa.class.php
@@ -63,6 +63,75 @@ class NHTSA {
   }
 
   /**
+   * Decode a 17-digit VIN and parse result
+   *
+   * @param string vin number
+   * @param ?integer model year
+   * @return ?array parsed NHTSA Year, Make, Model, Trim, Engine
+   * @throws TypeError
+   * @author Alec M. <https://amattu.com>
+   * @date 2021-04-04T16:36:29-040
+   */
+  public static function prettyDecode(string $vin, int $model_year = 0) : ?array
+  {
+    // Checks
+    if (!$vin || strlen($vin) != 17) {
+      return null;
+    }
+    if ($model_year && ($model_year < self::$minimum_year || $model_year > (date("Y") + 2))) {
+      return null;
+    }
+
+    // Fetch Data
+    $result = self::decodeVIN($vin, $model_year);
+    $parsed_result = Array();
+
+    // Parse Year
+    if (isset($result['Model Year']) && is_numeric($result['Model Year'])) {
+      $parsed_result["Model_Year"] = $result["Model Year"];
+    } else {
+      $parsed_result["Model_Year"] = null;
+    }
+
+    // Parse Make
+    if (isset($result['Make']) && !empty($result['Make'])) {
+      $parsed_result['Make'] = strtoupper($result['Make']);
+    }
+
+    // Parse Make
+    if (isset($result['Model']) && !empty($result['Model'])) {
+      $parsed_result['Model'] = strtoupper($result['Model']);
+    }
+
+    // Parse Trim
+    if (isset($result['Trim']) && !empty($result['Trim'])) {
+      $parsed_result['Trim'] = strtoupper($result['Trim']);
+    }
+
+    // Parse Engine
+    if (isset($result['Displacement (L)']) && !empty($result['Displacement (L)'])) {
+      $parsed_result['Engine'] = sprintf("%0.1f", $result['Displacement (L)']) ."L";
+    }
+    if (isset($result['Engine Number of Cylinders']) && !empty($result['Engine Number of Cylinders'])) {
+      $parsed_result['Engine'] .= " ". $result['Engine Number of Cylinders'] ."-Cyl";
+    }
+    if (isset($result['Fuel Type - Primary']) && !empty($result['Fuel Type - Primary']) && strtolower($result['Fuel Type - Primary']) == "diesel") {
+      $parsed_result['Engine'] .= " (Diesel)";
+    }
+    if (isset($result['Engine Model']) && !empty($result['Engine Model'])) {
+      $parsed_result['Engine'] .= " (". $result['Engine Model'] .")";
+    } else if (isset($result['Displacement (CC)']) && !empty($result['Displacement (CC)'])) {
+      $parsed_result['Engine'] .= " (". number_format($result['Displacement (CC)']) ."cc)";
+    }
+    if (isset($parsed_result['Engine'])) {
+      $parsed_result['Engine'] = strtoupper(preg_replace('/\s\s+/', ' ', $parsed_result['Engine']));
+    }
+
+    // Return
+    return !empty($parsed_result) ? $parsed_result : null;
+  }
+
+  /**
    * Perform a HTTP Get request
    *
    * @param string URL
